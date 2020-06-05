d806cfca9bb308e186e1f9732c94318e1afd7f19
カワモトセイヤ <kawamotoseiya@kawamotoseiyanoMacBook-Pro.local>
2020-06-05 18:31:15 +0900

    [削除機能]4-8

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -9,7 +9,6 @@ class BooksController < ApplicationController
 
   def new
   	  @book = Book.new
-
   end
 
   def create
@@ -25,6 +24,11 @@ class BooksController < ApplicationController
   	  book = Book.find(params[:id])
   	  book.update(book_params)
   	  redirect_to book_path(book)
+  end
+  def destroy
+  	  book = Book.find(params[:id])
+  	  book.destroy
+  	  redirect_to books_path
   end
 
   private
