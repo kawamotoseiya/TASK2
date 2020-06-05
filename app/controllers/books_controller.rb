26ea7d14a8fb842c90915068d53732fe9b2a4444
カワモトセイヤ <kawamotoseiya@kawamotoseiyanoMacBook-Pro.local>
2020-06-05 14:21:31 +0900

    [投稿機能]4-8

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,5 +1,6 @@
 class BooksController < ApplicationController
   def index
+  	  @books = Book.all
   end
 
   def show
@@ -7,12 +8,20 @@ class BooksController < ApplicationController
 
   def new
   	  @book = Book.new
-  	  
+
   end
 
   def create
+  	  book = Book.new(book_params)
+  	  book.save
+  	  redirect_to books_path
   end
 
   def edit
+  end
+
+  private
+  def book_params
+  	params.require(:book).permit(:title, :body)
   end
 end
